f63a0562c87ce152533b3a6d7141c5a91f9f9cac
Matt Yang <yang70@gmail.com>
2016-02-08 20:56:06 -0800

    passing tests for sections

--- spec/features/display_main_spec.rb
+++ spec/features/display_main_spec.rb
@@ -5,5 +5,33 @@ RSpec.feature "main page information" do
     visit root_path
 
     expect(page).to have_content("Romano Capital")
+    expect(page).to have_content("About")
+    expect(page).to have_content("Projects")
+    expect(page).to have_content("Let's Get In Touch!")
+    expect(page).to have_content("123 Fake Street")
+  end
+  
+  scenario  "display about section" do
+    visit root_path
+
+    expect(page).to have_content("About")
+  end
+
+  scenario  "display projects section" do
+    visit root_path
+
+    expect(page).to have_content("Projects")
+  end
+
+  scenario  "display contact section" do
+    visit root_path
+
+    expect(page).to have_content("Let's Get In Touch!")
+  end
+
+  scenario  "display footer" do
+    visit root_path
+
+    expect(page).to have_content("123 Fake Street")
   end
 end
