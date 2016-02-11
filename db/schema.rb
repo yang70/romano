b65a8eafd589b9d9ac10c927d2d24d320daa5500
Matt Yang <yang70@gmail.com>
2016-02-11 14:11:45 -0800

    create project resource, add table columns, migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160211220016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "address"
+    t.string   "image_url"
+    t.string   "status_plat_map_url"
+    t.string   "status"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+  end
 
 end
