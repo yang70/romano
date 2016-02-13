65a4e8f6bdba73205c61df9823b76158f428b567
Matt Yang <yang70@gmail.com>
2016-02-12 21:15:20 -0800

    update flash messages so errors display longer

--- app/assets/javascripts/flash.js
+++ app/assets/javascripts/flash.js
@@ -1,8 +1,14 @@
 
 $(document).on("ready page:change", function() {
   window.setTimeout(function() {
-      $(".alert").fadeTo(500, 0).slideUp(500, function(){
+      $(".alert-alert, .alert-error").fadeTo(500, 0).slideUp(500, function(){
           $(this).remove();
       });
   }, 5000);
+
+  window.setTimeout(function() {
+      $(".alert-success, .alert-notice").fadeTo(500, 0).slideUp(500, function(){
+          $(this).remove();
+      });
+  }, 2500);
 });
