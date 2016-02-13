ce1c906cb6520b7117ac0e73dce8de4d3f8ef44d
Matt Yang <yang70@gmail.com>
2016-02-13 13:51:43 -0800

    map of single project on show page working

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,10 @@
 class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
+    @hash = Gmaps4rails.build_markers(@project) do |project, marker|
+      marker.lat project.latitude
+      marker.lng project.longitude
+      marker.infowindow project.name
+    end
   end
 end
