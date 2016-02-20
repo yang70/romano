42edc0d78e820fc9800023c6d082cf920c7a60d7
Matt Yang <yang70@gmail.com>
2016-02-20 12:25:54 -0800

    remove fund progress circle form project, add button link to fund

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,6 +6,8 @@ class ProjectsController < ApplicationController
 
   def show
     @project = Project.find(params[:id])
+    @funds = @project.funds
+    @assets = @project.assets
     @fund_percentage = @project.fund_percentage
     @construction_percentage = @project.construction_percentage
     @hash = Gmaps4rails.build_markers(@project) do |project, marker|
