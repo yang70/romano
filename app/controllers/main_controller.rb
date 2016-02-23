bd9ddfe8505f84f0f20ab9a1227e3bc06a924761
Matt Yang <yang70@gmail.com>
2016-02-22 20:38:15 -0800

    fix copywright date in footer

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,6 +7,5 @@ class MainController < ApplicationController
       marker.lng project.longitude
       marker.infowindow '<h4><a class="map-link" href="/projects/' + project.id.to_s + '">' + project.name + '</a></h4>'
     end
-    @year = Date.today.year
   end
 end
