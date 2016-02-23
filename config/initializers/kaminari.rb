9f271d401f071ae4a84b099b30e24e987d529a78
Matt Yang <yang70@gmail.com>
2016-02-23 12:20:13 -0800

    add source for rails_admin fix

--- config/initializers/kaminari.rb
+++ config/initializers/kaminari.rb
@@ -1,3 +1,4 @@
+# Fix for rails_admin and will_paginate, per: https://github.com/sferik/rails_admin/wiki/Troubleshoot
 Kaminari.configure do |config|
   config.page_method_name = :per_page_kaminari
 end
