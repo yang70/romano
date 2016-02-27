6add9d6f38fb1e6b9f52b12f42dc46c21b8c0ae5
Matt Yang <yang70@gmail.com>
2016-02-26 22:24:28 -0800

    change default construction percentage to 0

--- db/migrate/20160214031248_add_construction_percentage_to_projects.rb
+++ db/migrate/20160214031248_add_construction_percentage_to_projects.rb
@@ -1,5 +1,5 @@
 class AddConstructionPercentageToProjects < ActiveRecord::Migration
   def change
-    add_column :projects, :construction_percentage, :integer
+    add_column :projects, :construction_percentage, :integer, default: 0
   end
 end
